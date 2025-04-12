3212fdd56acbc1b294ae0e2ebeeee9738ba7520e
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-13 02:07:26 +0600

    user module added

---
+++ practice-nest-js/src/users/dtos/create-user.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  Matches,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class createUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  lastName?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
+  @MinLength(8)
+  @Matches(/(?=.*[0-9])/, {
+    message: 'password must contain at least one number',
+  })
+  password: string;
+}
--- practice-nest-js/src/users/users.controller.ts
+++ practice-nest-js/src/users/users.controller.ts
@@ -2,23 +2,29 @@ import {
   Body,
   Controller,
   Get,
+  Header,
+  Headers,
+  Ip,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   Query,
   Req,
   Request,
+  ValidationPipe,
 } from '@nestjs/common';
+import { createUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get(':id')
-  public getUsers(@Param('id') id: string) {
-    return `getUsers(${id})`;
+  @Get(['','/:id'])
+  public getUsers(@Param('id', new ParseIntPipe({ optional: true })) id?: number) {
+    return typeof id;
   }
 
   @Post()
-  createUser(@Body() query: any) {
-    return query;
+  createUser(@Body() body:createUserDto) {
+    return body;
   }
 }
