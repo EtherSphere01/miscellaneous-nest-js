8ecbccd6f3b47eb5ab815f76d4811c47d498322f
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-13 00:50:43 +0600

    user module added

--- practice-nest-js/src/users/users.controller.ts
+++ practice-nest-js/src/users/users.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  Request,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers() {
-    return 'getUsers()';
+  @Get(':id')
+  public getUsers(@Param('id') id: string) {
+    return `getUsers(${id})`;
   }
 
-  @Put()
-  createUser() {
-    return 'createUser()';
+  @Post()
+  createUser(@Body() query: any) {
+    return query;
   }
 }
