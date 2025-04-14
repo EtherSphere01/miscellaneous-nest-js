51487ff606c7ddb42adc7c10b680cd5634448021
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-14 23:38:22 +0600

    entity added

--- practice-nest-js/src/app.module.ts
+++ practice-nest-js/src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: 5432,
+        username: 'postgres',
+        password: 'root',
+        host: 'localhost',
+        database: 'nestjs-blog',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
