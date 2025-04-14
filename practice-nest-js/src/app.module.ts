83efbdef0e5d1172e8e471271df5317081b72da4
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-04-15 02:51:18 +0600

    post moduel,entity created

--- practice-nest-js/src/app.module.ts
+++ practice-nest-js/src/app.module.ts
@@ -6,11 +6,13 @@ import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Post } from './posts/post-entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
+    ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRootAsync({
       imports: [],
       inject: [],
--- practice-nest-js/src/users/users.service.ts
+++ practice-nest-js/src/users/users.service.ts
@@ -1,35 +1,65 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { first } from 'rxjs';
 import { PassThrough } from 'stream';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createUserDto } from './dtos/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
     // injection of user repository
+
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+
+    // injecting config service
+
+    private readonly configService: ConfigService,
   ) {}
 
   public async createUser(createUserDto: createUserDto) {
-    // check is user already exists
-    const existingUser = await this.usersRepository.findOne({
-      where: { email: createUserDto.email },
-    });
+    let existingUser;
+    try {
+      // check is user already exists
+      existingUser = await this.usersRepository.findOne({
+        where: { email: createUserDto.email },
+      });
+    } catch (error) {
+      // handle error if user already exists
+      throw new RequestTimeoutException(
+        'User already exists with this email address',
+        {
+          cause: error,
+          description: 'User already exists with this email address',
+        },
+      );
+    }
+    if (existingUser) {
+      throw new BadRequestException(
+        'User already exists with this email address',
+      );
+    } else {
+      // handle exceptions if user already exists
 
-    // handle exceptions if user already exists
-
-    // create user
-    let user = this.usersRepository.create(createUserDto);
-    // save user to database
-    user = await this.usersRepository.save(user);
-    return user;
+      // create user
+      let user = this.usersRepository.create(createUserDto);
+      // save user to database
+      user = await this.usersRepository.save(user);
+      return user;
+    }
   }
 
   public findAll() {
+    const environment = this.configService.get<string>('S3_BUCKET');
+
     return [
       {
         id: 1,
